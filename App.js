c44019bbefb5ce7e86d9bff29459258052ec16d4
Rinat Khusainov <khusainovrm@yandex.ru>
2020-11-07 00:18:54 +0300

    feat: alias

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import {tailwind} from './tailwind';
+import { Text, View, StyleSheet } from 'react-native'
+import { tailwind, getColor } from 'tailwind'
+import BottomMenu from 'components/BottomMenu'
 
+const primary100 = tailwind('text-primary-100').color
 
 export default function App() {
   return (
-    <View  style={tailwind('h-full')}>
-		<View style={tailwind('pt-12 items-center')}>
-			<View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
-				<Text style={tailwind('text-primary-500 font-semibold')}>
-					Hello Tailwind
-				</Text>
-			</View>
-		</View>
-	</View>
-  );
+    <View style={styles.container}>
+      <View style={tailwind('items-center')}>
+        <View style={tailwind('bg-blue-200 py-1 rounded-full mt-12')}>
+          <Text style={tailwind('text-primary-500 font-semibold')}>
+            Hello Tailwind
+          </Text>
+        </View>
+      </View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Text>Заголовок</Text>
+        <iframe
+        style={tailwind('w-full')}
+          // src="https://facecast.net/v/5d4bpk"
+        ></iframe>
+      </View>
+
+      <View style={tailwind('fixed bottom-0 left-0 right-0 bg-white h-20 w-full')}>
+        <BottomMenu/>
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: primary100,
+    height: 100,
+    alignItems: 'center',
+  },
+
+})
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    "env": {
+      "development": {
+        "plugins":
+          ["@babel/plugin-transform-react-jsx-source",
+            ["module-resolver",{
+                "@": ["./src"],
+                "alias": {
+                  "tailwind": "./tailwind.js",
+                  "components": "./src/components",
+                }
+              }
+            ]
+          ]
+        }
+      }
   };
 };
